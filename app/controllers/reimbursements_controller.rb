d4dccd6bfc4016955c0b3a30c4009e76418dce72
Michael Lichenstein <mclich01@gmail.com>
2023-04-05 16:32:09 -0400

    project updates

--- app/controllers/reimbursements_controller.rb
+++ app/controllers/reimbursements_controller.rb
@@ -1,2 +1,50 @@
 class ReimbursementsController < ApplicationController
+
+  def index
+    @reimbursements = Reimbursement.all
+  end
+
+
+  def destroy
+    @reimbursement = Reimbursement.find(params[:id])
+    @reimbursement.destroy
+    redirect_to @reimbursement
+  end
+
+  def edit
+    @reimbursement = Reimbursement.find(params[:id])
+  end
+
+  def update
+    @reimbursement = Reimbursement.find(params[:id])
+    if @reimbursement.update(reimbursement_params)
+      redirect_to @reimbursement
+    else
+      #edit instead of update??
+      render :update, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @reimbursement = current_user.reimbursement.new
+  end
+
+  def create
+    @reimbursement = current_user.reimbursement.new(reimbursement_params)
+    if @reimbursement.save
+      redirect_to @reimbursement
+    else
+      #new instead of update??
+      render :update, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reimbursement_params
+    params.require(:reimbursement).permit(:amount, :description, :status)
+  end
+
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,3 +3,6 @@ class UsersController < ApplicationController
 
   end
 end
+def index
+  @users = User.all
+end
